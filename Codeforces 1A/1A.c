d8dd9f78e0a6c4bb1901038a841c8cb1d3bcd7d9
Karoui fares <karouifares1@gmail.com>
2025-12-20 16:00:46 +0100

    first commit

---
+++ Codeforces 1A/1A.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    while(k>0)
+    {
+        if(n%10==0)
+            n=n/10;
+        else 
+            n--;
+        k--;
+    }
+    printf("%d",n);
+
+    return 0;
+}
